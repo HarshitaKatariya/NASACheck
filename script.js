15d11b11244ce533bfae66e6b0624110fca52371
HarshitaKatariya <harshitakatariya1508@gmail.com>
2024-10-03 17:17:50 +0530

    This is a commit1 for check

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const dropdown = document.querySelector('.dropdown');
+//     // const dropdownContent = document.querySelector('.dropdown-content');
+  
+//     // Toggle dropdown when "Planets" is clicked
+//     dropdown.addEventListener('click', function (event) {
+//       this.classList.toggle('active');
+//       event.stopPropagation(); // Prevent click from bubbling to the document
+//     });
+  
+//     // Close the dropdown when clicking outside
+//     document.addEventListener('click', function (event) {
+//       if (!dropdown.contains(event.target)) {
+//         dropdown.classList.remove('active');
+//       }
+//     });
+//   });
+  
